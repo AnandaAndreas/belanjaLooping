39c68257e70174ce879eccccee6debd27c9f6d8c
AnandaAndreas <anandaandreas@students.amikom.ac.id>
2023-03-31 02:18:23 +0700

    nota update

--- src/main/kotlin/belanjaLooping.kt
+++ src/main/kotlin/belanjaLooping.kt
@@ -8,37 +8,46 @@ fun daftarBelanja(){
 }
 fun main() {
     daftarBelanja()
-    var harga = 0
+    var harga = mutableListOf<Int>()
     var total = 0
+    var x = 0
     do {
         print("Masukan Menu : ")
         var pilih = readln()
         when (pilih) {
             "1" -> { println("Anda Membeli Fanta dengan harga 7.000")
-                harga = 7000
-                total += harga  }
+                harga.add(7000)
+                total += harga[x]
+                x++ }
             "2" -> { println("Anda Membeli Mie sedap dengan harga 3.500")
-                harga = 3500
-                total += harga }
+                harga.add(3500)
+                total += harga[x]
+                x++ }
             "3" -> { println("Anda Membeli Nutrisari dengan harga 2.000")
-                harga = 2000
-                total += harga }
+                harga.add(2000)
+                total += harga[x]
+                x++ }
             "4" -> { println("Anda Membeli Coca-Cola dengan harga 7.500")
-                harga = 7500
-                total += harga }
+                harga.add(7500)
+                total += harga[x]
+                x++ }
             "5" -> { println("Anda Membeli Indomie dengan harga 3.500")
-                harga = 3500
-                total += harga }
+                harga.add(3500)
+                total += harga[x]
+                x++ }
             "6" -> { println("Anda Membeli GoodDay dengan harga 2.500")
-                harga = 2500
-                total += harga }
+                harga.add(2500)
+                total += harga[x]
+                x++ }
             "7" -> println("Terima kasih Sudah belanja di sini")
         }
     }
         while (pilih != "7")
         println("====barang yang anda beli adalah=====")
-        println("barang1 : harga")
-        println("barang2 : harga")
+        for ((i,b) in harga.withIndex()){
+            var nomer = i + 1
+            println("harga barang ke $nomer adalah $b")
+        }
         println("Total yang harus anda bayar : $total")
 
 }
