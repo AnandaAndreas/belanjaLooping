e2b69bdaea41c8482ed44af9636b3caae252e3d3
AnandaAndreas <anandaandreas@students.amikom.ac.id>
2023-03-31 03:05:48 +0700

    nota update2

--- src/main/kotlin/belanjaLooping.kt
+++ src/main/kotlin/belanjaLooping.kt
@@ -8,44 +8,51 @@ fun daftarBelanja(){
 }
 fun main() {
     daftarBelanja()
-    var harga = mutableListOf<Int>()
+    var totalHarga = mutableListOf<Int>()
     var total = 0
     var x = 0
+    var harga = 0
     do {
         print("Masukan Menu : ")
         var pilih = readln()
         when (pilih) {
-            "1" -> { println("Anda Membeli Fanta dengan harga 7.000")
-                harga.add(7000)
-                total += harga[x]
+            "1" -> { println("Anda Membeli Fanta dengan harga @7.000")
+                print("Masukan Jumlah beli : ")
+                var jumlahBeli = readln().toInt()
+                harga = 7000
+                totalHarga.add(jumlahBeli * harga)
+                total += totalHarga[x]
                 x++ }
             "2" -> { println("Anda Membeli Mie sedap dengan harga 3.500")
-                harga.add(3500)
-                total += harga[x]
+                print("Masukan Jumlah beli : ")
+                var jumlahBeli = readln().toInt()
+                harga = 3500
+                totalHarga.add(jumlahBeli * harga)
+                total += totalHarga[x]
                 x++ }
-            "3" -> { println("Anda Membeli Nutrisari dengan harga 2.000")
-                harga.add(2000)
-                total += harga[x]
-                x++ }
-            "4" -> { println("Anda Membeli Coca-Cola dengan harga 7.500")
-                harga.add(7500)
-                total += harga[x]
-                x++ }
-            "5" -> { println("Anda Membeli Indomie dengan harga 3.500")
-                harga.add(3500)
-                total += harga[x]
-                x++ }
-            "6" -> { println("Anda Membeli GoodDay dengan harga 2.500")
-                harga.add(2500)
-                total += harga[x]
-                x++ }
+//            "3" -> { println("Anda Membeli Nutrisari dengan harga 2.000")
+//                harga.add(2000)
+//                total += harga[x]
+//                x++ }
+//            "4" -> { println("Anda Membeli Coca-Cola dengan harga 7.500")
+//                harga.add(7500)
+//                total += harga[x]
+//                x++ }
+//            "5" -> { println("Anda Membeli Indomie dengan harga 3.500")
+//                harga.add(3500)
+//                total += harga[x]
+//                x++ }
+//            "6" -> { println("Anda Membeli GoodDay dengan harga 2.500")
+//                harga.add(2500)
+//                total += harga[x]
+//                x++ }
             "7" -> println("Terima kasih Sudah belanja di sini")
         }
     }
         while (pilih != "7")
         println("====barang yang anda beli adalah=====")
-        for ((i,b) in harga.withIndex()){
-            var nomer = i + 1
+        for (b in totalHarga){
+            var nomer = totalHarga.indexOf(b) + 1
             println("harga barang ke $nomer adalah $b")
         }
         println("Total yang harus anda bayar : $total")
